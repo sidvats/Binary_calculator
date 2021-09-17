8d7575932bf4176487af0a0f4bebc1dac1fd695d
sidvats <sidkishore.vats@gmail.com>
2021-09-17 21:11:18 +0530

    v-2

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -35,13 +35,42 @@ document.getElementById('btnDiv').addEventListener('click',()=>{
 function solve(){
     var val=document.getElementById('res').innerHTML;
     let re=/(\+|\-|\*|\/)/g;
-    var operators=val.match(re)
+    var operators=val.match(re);
     var values=val.split(operators[0]);
-    console.log(values);
+    var operand1=condec(values[0]);
+    var operand2=condec(values[1]);
+    // var temp;
+    // operand1 > operand2 ? operand1 : ()=>{
+    //     temp=operand1;
+    //     operand1=operand2;
+    //     operand2=temp;
+    // };
+    if(operators[0]=='+')
+        var resultdec=operand1 + operand2;
+    else if(operators[0]=='-')
+        var resultdec=operand1 - operand2;    
+    else if(operators[0]=='*')
+        var resultdec=operand1 * operand2;    
+    else
+        var resultdec=parse(operand1 / operand2);    
+        
+    var resultbin=conbin(resultdec);
+    var r=document.getElementById('res').innerText;
+    document.getElementById('res').innerText=r+'='+resultbin;
 }
 function conbin(num){
+var result=num.toString(2);
 return result;
 }
-function condec(num){
-return result;
+// function condec(num){
+//     var count=num.toString().split('').length;
+//     var n=num.split('');
+//     alert(n);
+//     var result=0;
+//     alert(result);
+// return result;
+// }
+function condec(bstr) { 
+    return parseInt((bstr + '')
+    .replace(/[^01]/gi, ''), 2);
 }
